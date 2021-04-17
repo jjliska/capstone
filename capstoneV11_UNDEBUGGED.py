7adda8f6ba06a19f7fa381b3691e22478d138bdc
Joshua Liska <>
2021-04-17 15:25:09 -0700

    Update

--- capstoneV11_UNDEBUGGED.py
+++ capstoneV11_UNDEBUGGED.py
@@ -49,9 +49,7 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 #Serial Communication
 #---------------------------------------------------------------------------------------------------------
 try:
-  ser = serial.Serial(
-    port='COM3'
-  )
+  ser = serial.Serial(port='COM3')
   ser.flush()
 except:
   print("Serial port is not connected")
